7cc8b243fb2148e9e062df746561951e268ce878
EstebanHernandezA16 <147927980+Criss-Fxck@users.noreply.github.com>
2023-11-25 08:55:38 -0500

    Update Funcionalidad.js

--- Funcionalidad.js
+++ Funcionalidad.js
@@ -13,6 +13,10 @@ function validarTiempo(inputId) {
   }
 }
 
+/* La explicacion que le damos: Es una función sencilla  que  utilizamos para 
+validar que el valor introducido en un input de tipo time, validamos que sea un número 
+válido mayor que cero. */
+
 function iniciarCronometro(cronometroId, mensajeId, inputId) {
   if (validarTiempo(inputId)) {
     let tiempo = parseInt(document.getElementById(inputId).value);
@@ -34,11 +38,36 @@ function iniciarCronometro(cronometroId, mensajeId, inputId) {
   }
 }
 
+/* La explicacion de lo que hace la funcion es: 
+- Valida que el valor introducido en el input de tipo time sea un número 
+válido mayor que cero
+- Si el valor es válido, obtiene el tiempo especificado por el usuario
+- Inicializa el contador de segundos a 0
+- Actualiza el mensaje de estado del cronómetro a "Ejecutándose"
+- Inicia un intervalo de 1000 milisegundos (1 segundo) que se ejecutará 
+continuamente por el tiempo especificado por el usuario
+- Dentro del intervalo, actualiza el texto del elemento cronometroId con el 
+tiempo que esta transcurriendo
+- Si el tiempo transcurrido es menor que el tiempo especificado por el usuario, 
+incrementa el contador de segundos
+- Si el tiempo transcurrido es igual o mayor que el tiempo especificado por el 
+usuario, detiene el intervalo y actualiza el mensaje de estado del cronómetro 
+a "En Pausa" */
+
 function pausarCronometro(cronometroId, mensajeId) {
   clearInterval(intervalos[cronometroId]);
   intervalos[cronometroId] = null;
   document.getElementById(mensajeId).innerText = 'En Pausa';
 }
+
+/* Explicacion: La funcion tiene como objetivo pausar un cronómetro. La función 
+funciona de la siguiente manera:
+- Obtiene el identificador del intervalo del cronómetro que se desea pausar
+- Detiene el intervalo del cronómetro
+- Establece la propiedad intervalos del cronómetro a null
+- Actualiza el mensaje de estado del cronómetro a "En Pausa"
+- Si el cronómetro está en ejecución, la función pausarCronometro() lo detendrá 
+y actualizará el mensaje de estado a "En Pausa" */
 
 function reiniciarCronometro(cronometroId, mensajeId, inputId) {
   clearInterval(intervalos[cronometroId]);
@@ -47,10 +76,16 @@ function reiniciarCronometro(cronometroId, mensajeId, inputId) {
   document.getElementById(mensajeId).innerText = 'Iniciado';
   document.getElementById(inputId).value = "";
 }
+/* Explicacion de lo que hace la funcion:
+- Detiene el intervalo del cronómetro
+- Establece la propiedad intervalos del cronómetro a null
+- Establece el texto del elemento cronometroId a 00:00
+- Establece el mensaje de estado del cronómetro a 'Iniciado'
+- Establece el valor del input de tipo time */
 
 
-/* No e utiliza. INTENTO FALLIDO. */
-/* function iniciarTodosLosCronometros() {
+
+ function iniciarTodosLosCronometros() {
   if (validarTiempo('inputTiempo1')) {
     iniciarCronometro('cronometro1','mensaje1','inputTiempo1');
     iniciarCronometro('cronometro2','mensaje2','inputTiempo2');
@@ -58,6 +93,8 @@ function reiniciarCronometro(cronometroId, mensajeId, inputId) {
   }
 }
 
+/* No e utiliza. INTENTO FALLIDO. */
+/*
 function pausarTodosLosCronometros() {
   pausarCronometro('cronometro1','mensaje1');
   pausarCronometro('cronometro2','mensaje2');
